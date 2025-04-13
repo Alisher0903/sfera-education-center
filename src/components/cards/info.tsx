0113982a483a7db60ddc98af807957e8fd3d6ac8
Alisher0903 <alishersodiqov09@gmail.com>
2025-04-13 18:48:05 +0500

    bag fixed 2

--- src/components/cards/info.tsx
+++ src/components/cards/info.tsx
@@ -7,13 +7,13 @@ export function InfoCard() {
         <div className="w-6 h-6 rounded-full border border-gray-800 flex items-center justify-center">
           <Info className="h-3.5 w-3.5" />
         </div>
-        <h2 className="text-2xl font-medium text-gray-900">Ta'lim haqida</h2>
+        <h2 className="text-2xl font-medium text-gray-900">Ta&apos;lim haqida</h2>
       </div>
 
       <ul className="space-y-5">
         <li className="flex items-start">
           <span className="text-gray-400 mr-3 mt-1">•</span>
-          <span className="text-gray-600">Ta'lim guruhda olib boriladi.</span>
+          <span className="text-gray-600">Ta&apos;lim guruhda olib boriladi.</span>
         </li>
         <li className="flex items-start">
           <span className="text-gray-400 mr-3 mt-1">•</span>
@@ -21,7 +21,7 @@ export function InfoCard() {
         </li>
         <li className="flex items-start">
           <span className="text-gray-400 mr-3 mt-1">•</span>
-          <span className="text-gray-600">Bir oyda 8 ta mashg'ulot bo'ladi.</span>
+          <span className="text-gray-600">Bir oyda 8 ta mashg&apos;ulot bo&apos;ladi.</span>
         </li>
         <li className="flex items-start">
           <span className="text-gray-400 mr-3 mt-1">•</span>
@@ -33,7 +33,7 @@ export function InfoCard() {
         </li>
         <li className="flex items-start">
           <span className="text-gray-400 mr-3 mt-1">•</span>
-          <span className="text-gray-600">Haftasiga 2 ta dars o'tiladi.</span>
+          <span className="text-gray-600">Haftasiga 2 ta dars o&apos;tiladi.</span>
         </li>
       </ul>
     </div>
