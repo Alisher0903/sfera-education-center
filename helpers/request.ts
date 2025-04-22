41b7cb3675742ceae875c29311fdf6ec360ff1aa
Alisher0903 <alishersodiqov09@gmail.com>
2025-04-22 15:04:50 +0500

    full bag fixed

--- helpers/request.ts
+++ helpers/request.ts
@@ -1,6 +1,15 @@
-export async function getAllData<T = any>(url: string): Promise<T> {
-    const res = await fetch(url, {
-        cache: 'no-store',
-    });
-    return res.json();
+export async function getAllData<T = any>(url: string): Promise<T | null> {
+    try {
+        const res = await fetch(url, {
+            cache: "no-store",
+        });
+        if (!res.ok) {
+            console.error(`Error fetching ${url}:`, res.status);
+            return null;
+        }
+        return await res.json();
+    } catch (error) {
+        console.error(`Error fetching ${url}:`, error);
+        return null;
+    }
 }
